a7d307917f06c00e1906e1647f5ce054631d56de
Souvik Maity <souvik@squadrun.co>
2018-09-08 17:04:36 +0530

    all

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/souvik/react-sites/first-app/src/layouts/index.js"
+  import data from "/Users/souvik/react-sites/first-app/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
